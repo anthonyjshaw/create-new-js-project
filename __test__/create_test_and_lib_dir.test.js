803a00cdcd6bf5d5b65dcf63984887402cd6b8a9
Anthony Shaw <anthonyshaw@sceaga.com>
2021-11-12 10:42:13 +0000

    Added template file

--- __test__/create_test_and_lib_dir.test.js
+++ __test__/create_test_and_lib_dir.test.js
@@ -2,7 +2,7 @@ const createTestAndLibDir = require('../lib/create_test_and_lib_dir');
 const fs = require('fs');
 
 describe('createProjectDir', () => {
-	test('should create directory marked test', async () => {
+	test('should create a directory marked test', async () => {
 	 	createTestAndLibDir('./__test__/example')
 		const dir = './__test__/example/__test__';
 		await expect(fs.existsSync(dir)).toBe(true);
---
+++ __test__/lib/template_strings.js
@@ -0,0 +1,3 @@
+const templateFileContent = require('../../lib/template_file_content');
+
+const templateStrings = templateFileContent('small_man')

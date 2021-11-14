026202bf3f9e624674272c98a80e9ec802bd3886
Anthony Shaw <anthonyshaw@sceaga.com>
2021-11-14 19:16:14 +0000

    Updated the create_files.test.js

--- __test__/create_files.test.js
+++ __test__/create_files.test.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 const templateFileContent = require("../lib/template_file_content");
 const createTestAndLibDir = require("../lib/create_test_and_lib_dir");
 
+afterAll(() => {
+	const testProjects = ['test_project', 'test_project1', 'test_project2'];
+	testProjects.forEach(e => {
+		fs.rmdirSync(`./${e}`, { recursive: true, force: true });
+	})
+})
 
 describe('createFiles', () => {
 	test('should create a file in the lib directiory with a template function', () => {
@@ -23,6 +29,13 @@ describe('createFiles', () => {
 		expect(fs.existsSync(projectTestFile)).toBe(true);
 	});
 
-
+	test('should create a package.json with jest installed', () => {
+		const projectName = 'test_project2';
+		createTestAndLibDir(projectName);
+		const content = templateFileContent(projectName);
+		createFiles(projectName, content);
+		const packageJson = `${projectName}/package.json`;
+		expect(fs.existsSync(packageJson)).toBe(true);
+	});
 	
 })

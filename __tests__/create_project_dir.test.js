18fbbef036f57f3d36401d2460e54f14bc671d44
Anthony Shaw <anthonyshaw@sceaga.com>
2021-11-14 23:46:06 +0000

    refactored create-js-project file

--- __tests__/create_project_dir.test.js
+++ __tests__/create_project_dir.test.js
@@ -1,15 +1,14 @@
+/* eslint-disable no-undef */
 const createProjectDir = require('../lib/create_project_dir');
 const fs = require('fs');
 
-
 afterAll(() => {
-	fs.rmdirSync('./__tests__/example', {recursive: true, force: true})
-})
-
+	fs.rmdirSync('./__tests__/example', {recursive: true, force: true});
+});
 
 describe('createProjectDir', () => {
 	test('should create directory marked example', async () => {
-	 	createProjectDir('__tests__/example');
+		createProjectDir('__tests__/example');
 		const dir = './__tests__/example/';
 		expect(await fs.existsSync(dir)).toBe(true);
 	});
